aedb82a28e0905a0c00bb2fc4ccbf4667d761c3c
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2025-05-19 01:04:40 +0000

    shh, no errors when not connected

--- powershell/disconnect.ps1
+++ powershell/disconnect.ps1
@@ -1,4 +1,18 @@
-Disconnect-MgGraph
-Disconnect-ExchangeOnline -Confirm:$false
+
+if ((Get-Command Disconnect-MgGraph -errorAction SilentlyContinue) -eq $null) {
+    Write-Host "Did not find Microsoft Graph disconnect cmdlet"
+}
+else
+{
+    Disconnect-MgGraph
+}
+
+if ((Get-Command Disconnect-ExchangeOnline -errorAction SilentlyContinue) -eq $null) {
+    Write-Host "Did not find Exchange online disconnect cmdlet"
+}
+else
+{
+    Disconnect-ExchangeOnline -Confirm:$false
+}
 
 Write-Host "Done"
