e825be01e705fd0c4c08188f9179cca189fa3f9a
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2025-05-30 00:59:57 +0000

    masively overcomplicating this

--- bash/git_pull_all.sh
+++ bash/git_pull_all.sh
@@ -1,5 +1,33 @@
-files=(*)
-for file in "${files[@]}"; do
-	printf "$(pwd)/$file\n"
-	git -C "$(pwd)/$file" pull
-done
+function __git_pull_all
+{
+	local files=(*);
+	local filepath="";
+	local errors=();
+	for file in "${files[@]}"; do
+		filepath="$(pwd)/$file";
+		if [[ -f "$filepath" ]]; then
+			printf "\033[7m$file\033[0m is a file so cannot be a git repo, skipping\n";
+			continue
+		fi
+		if [[ ! -d "$filepath/.git" ]]; then
+			printf "\033[7m$file\033[0m does not have folder .git inside, skipping\n";
+			continue
+		fi
+		printf "\033[7m$file\033[0m "
+		# printf "AAAA\n";
+		if git -C "$(pwd)/$file" pull ; then
+			printf "";
+		else
+			errors+=($file)
+		fi
+	done
+	if [[ ${#errors[@]} > 0 ]]; then
+		printf "${red}Failed to update:${normal}\n";
+		for error in "${errors[@]}"; do
+			printf "\t$error\n";
+		done
+	fi
+}
+
+__git_pull_all
+unset __git_pull_all
