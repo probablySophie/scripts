0f65210af1bc7ad3771350f1181be4fc94986b33
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2025-05-30 01:02:20 +0000

    using .env values & connecting to PnP

--- powershell/connect.ps1
+++ powershell/connect.ps1
@@ -1,8 +1,13 @@
 param($param1)
 
+$ENV_FILE = ".env"
+get-content $ENV_FILE | foreach {
+    $name, $value = $_.split('=')
+    set-content env:\$name $value
+}
+
 function Connect-Mg
 {
-    Import-Module Microsoft.Graph
     Connect-MgGraph -Scopes "User.ReadWrite.All","Group.ReadWrite.All"
 }
 
@@ -10,8 +15,18 @@ if ( $PSBoundParameters.Keys.Count -eq 1 )
 {
     switch ($param1)
     {
-        "exchange" { Connect-ExchangeOnline -Device }
-        "graph" { Connect-MgGraph -Scopes "User.ReadWrite.All","Group.ReadWrite.All" }
+        "exchange" {
+
+            Connect-ExchangeOnline -Device
+        }
+        "graph" {
+            Import-Module Microsoft.Graph
+            Connect-MgGraph -Scopes "User.ReadWrite.All","Group.ReadWrite.All"
+        }
+        "pnp" {
+            Import-Module PnP.PowerShell
+            Connect-PnPOnline -Url "$env:SharePointTenant.sharepoint.com" -Interactive  -ClientId "$env:PNPClientID"
+        }
     }
 }
 else
@@ -19,4 +34,5 @@ else
     Write-Host "Options: "
     Write-Host "`t exchange "
     Write-Host "`t graph    "
+    Write-Host "`t pnp      "
 }
