7f0c470c74da2fb1caac140e03d62697f519c4dd
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2025-07-14 11:45:19 +0800

    improvements

--- powershell/connect.ps1
+++ powershell/connect.ps1
@@ -11,18 +11,19 @@ if ( Test-Path $ENV_FILE ) {
 
 $GRAPH_SCOPES="User.ReadWrite.All","Group.ReadWrite.All"
 
-if ( $PSBoundParameters.Keys.Count -eq 1 )
+function Connect
 {
+    param($connectionChoice, $suppressWelcome)
     switch ($connectionChoice)
     {
         "exchange" {
-
+            Write-Host "Connecting to Exchange Online..."
             Connect-ExchangeOnline -Device
         }
         "graph" {
             # Write-Host "Importing Graph..."
             # Import-Module Microsoft.Graph
-            Write-Host "Connecting..."
+            Write-Host "Connecting to Microsoft Graph..."
             if (($suppressWelcome -ne $null ) -and ($suppressWelcome -eq $true))
             {
                 Connect-MgGraph -Scopes $GRAPH_SCOPES -NoWelcome -UseDeviceCode
@@ -33,10 +34,17 @@ if ( $PSBoundParameters.Keys.Count -eq 1 )
             }
         }
         "pnp" {
+            Write-Host "Connecting to Microsoft PnP..."
             Import-Module PnP.PowerShell
             Connect-PnPOnline -Url "$env:SharePointTenant.sharepoint.com" -Interactive  -ClientId "$env:PNPClientID"
         }
     }
+    
+}
+
+if ( $PSBoundParameters.Keys.Count -eq 1 )
+{
+    Connect "$connectionChoice" "$suppressWelcome"
 }
 else
 {
@@ -44,4 +52,15 @@ else
     Write-Host "`t exchange "
     Write-Host "`t graph    "
     Write-Host "`t pnp      "
+
+    $options = [System.Management.Automation.Host.ChoiceDescription[]](
+        (New-Object System.Management.Automation.Host.ChoiceDescription "&Graph", "Microsoft Graph"),
+        (New-Object System.Management.Automation.Host.ChoiceDescription "&Exchange", "Microsoft Exchange"),
+        (New-Object System.Management.Automation.Host.ChoiceDescription "&PnP", "Microsoft PnP") )
+    $result = $host.ui.PromptForChoice("Connect", "Which service would you like to connect to?", $options, 0)
+    switch ($result) {
+        0 { Connect "graph" }
+        1 { Connect "exchange" }
+        2 { Connect "pnp" }
+    }
 }
