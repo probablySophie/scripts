6b1630d1fd0935c3d03a45deff909ac5e53af4a2
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2025-07-14 11:45:46 +0800

    ehhh

--- powershell/get_pnp.ps1
+++ powershell/get_pnp.ps1
@@ -24,44 +24,49 @@ function print_if_not_null
     }
 }
 
-switch ($choice.ToLower())
+function get_pnp()
 {
-    "site" {
-        # Connect to a given site URL
-        Connect-PnPOnline -Url "$(Read-Host -Prompt 'Site URL')" -Interactive  -ClientId "$env:PNPClientID"
-        $Global:pnpsite = Get-PnpWeb
-        Write-Host "Saved site into `$pnpsite";
-    }
+    param($choice)
+    switch ($choice.ToLower())
+    {
+        "site" {
+            # Connect to a given site URL
+            Connect-PnPOnline -Url "$(Read-Host -Prompt 'Site URL')" -Interactive  -ClientId "$env:PNPClientID"
+            $Global:pnpsite = Get-PnpWeb
+            Write-Host "Saved site into `$pnpsite";
+        }
 
-    "list" {
-        # Make sure there's a site to pull from
-        if ( $pnpsite -eq $null ) { Write-Host "`$pnpsite is null!"; return }
+        "list" {
+            # Make sure there's a site to pull from
+            if ( $pnpsite -eq $null ) { Write-Host "`$pnpsite is null!"; get_pnp "site"; }
         
-        $__filter = Read-Host -Prompt 'Display Name Filter (blank for all)';
+            $__filter = Read-Host -Prompt 'Display Name Filter (blank for all)';
 
-        $pnplists = Get-PnpList | Where-Object { $_.Title -like "*$($__filter)*" }
+            $pnplists = Get-PnpList | Where-Object { $_.Title -like "*$($__filter)*" }
         
-        $pnplists `
-            | % {$i=0}{ [PsCustomObject]@{Num=$i;Name=$_.Title;} ;$i++} `
-            | Format-Table;
-        # Then ask the user which index they'd like
-        $Global:pnplist = $pnplists[$(Read-Host -Prompt "Your chosen list's num")];
-        Write-Host "Saved '$($pnplist.Title)' into `$pnplist";
-    }
+            $pnplists `
+                | % {$i=0}{ [PsCustomObject]@{Num=$i;Name=$_.Title;} ;$i++} `
+                | Format-Table;
+            # Then ask the user which index they'd like
+            $Global:pnplist = $pnplists[$(Read-Host -Prompt "Your chosen list's num")];
+            Write-Host "Saved '$($pnplist.Title)' into `$pnplist";
+        }
 
-    "listitems" {
-        # Make sure there's a site & a list to pull from
-        if ( $pnpsite -eq $null ) { Write-Host "`$pnpsite is null!"; return }
-        if ( $pnplist -eq $null ) { Write-Host "`$pnplist is null!"; return }
+        "listitems" {
+            # Make sure there's a site & a list to pull from
+            if ( $pnplist -eq $null ) { Write-Host "`$pnplist is null!"; get_pnp "list"; }
 
-        $Global:pnplistitems = Get-PnpListItem -List $pnplist;
-        Write-Host "Saved $($pnplistitems.Length) items from '$($pnplist.Title)' into `$pnplistitems";
-    }
+            $Global:pnplistitems = Get-PnpListItem -List $pnplist;
+            Write-Host "Saved $($pnplistitems.Length) items from '$($pnplist.Title)' into `$pnplistitems";
+        }
 
-    # Remember to add any nre items to the start of file options list!
-    default {
-        __found_nothing
+        # Remember to add any nre items to the start of file options list!
+        default {
+            __found_nothing
+        }
     }
 }
+
+get_pnp $choice
 
 
