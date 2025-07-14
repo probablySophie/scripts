b089285db72c5b376c67422f0553d23b3ee8936c
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2025-07-14 11:45:36 +0800

    major improvements

--- powershell/get_graph.ps1
+++ powershell/get_graph.ps1
@@ -8,72 +8,81 @@ function __found_nothing
     Write-Host "`t list";
     Write-Host "`t listitems"
 }
+
+
+
+function GetGraph
+{
+    param($choice)
+    switch ($choice.ToLower())
+    {
+        "site" {
+            # Get a site name from the user & search for it
+            $sites = Get-MgSite -search "$(Read-Host -Prompt 'Site Display Name')";
+            # Filter down to one site
+            $result = . "./utils/sp_chooseFromList.ps1" $sites "Site"
+            if ( $result -ne $null ) {
+                $Global:site = $result;
+                Write-Host "The variable `$site now contains the site: $($Global:site.displayName)"
+            }
+            else { Write-Host "Something went wrong. Did not change variable `$site" }
+            return
+        }
+
+        "list" {
+            if ($Global:site -eq $null) { GetGraph "site" }
+            else {
+                $result = . "./utils/choice.ps1" @( @("Yes", "Yes"), @("No", "No") ) "Use Site?" "Would you like to use the Site $($Global:site.DisplayName)?";
+                if ( $result -eq 1 ) { GetGraph "site" }
+            }
+
+            # Get a site name from the user & search for it
+            $lists = Get-MgSiteList -SiteId $Global:site.id -All;
+            # Filter down to one site
+            $result = . "./utils/sp_chooseFromList.ps1" $lists "List"
+            if ( $result -ne $null ) {
+                $Global:list = $result;
+                Write-Host "The variable `$list now contains the list: $($result.displayName)"
+            }
+            else { Write-Host "Something went wrong. Did not change variable `$list" }
+            return
+        }
+
+        "listitems" {
+            if ($Global:list -eq $null) { Write-Host "`$list is empty"; GetGraph "list" }
+            else {
+                $result = . "./utils/choice.ps1" @( @("Yes", "Yes"), @("No", "No") ) "" "Would you like to get items from the list $($Global:list.DisplayName)?";
+                if ( $result -eq 1 ) { GetGraph "list" }
+            }
+            
+            $Global:listItems = Get-MgSiteListItem -SiteId $site.id -ListId $list.id -All -ExpandProperty Fields
+            Write-Host "Stored $($listItems.length) items from '$($list.DisplayName)' into `$listItems"
+            return
+        }
+
+        # Remember to add any nre items to the start of file options list!
+        default {
+            __found_nothing
+        }
+    }
+}
+
 if ( $choice -eq $null ) {
     __found_nothing
-    return
+
+    $options = [System.Management.Automation.Host.ChoiceDescription[]](
+        (New-Object System.Management.Automation.Host.ChoiceDescription "&Site", "A SharePoint Site"),
+        (New-Object System.Management.Automation.Host.ChoiceDescription "&List", "A SharePoint Site List"),
+        (New-Object System.Management.Automation.Host.ChoiceDescription "&Items", "Items from a SharePoint list") )
+    $result = $host.ui.PromptForChoice("Get-Graph", "Which would you like to get?", $options, 0)
+    switch ($result) {
+        0 { GetGraph "site" }
+        1 { GetGraph "list" }
+        2 { GetGraph "listitems" }
+    }
+} else {
+    GetGraph "$choice"
 }
-
-function print_if_not_null
-{
-    param($value, $variable_name)
-    if ( $value -eq $null ) {
-        Write-Host "Something went wrong!  `$$variable_name is null";
-    } else {
-        $value
-        Write-Host "Storing in `$$variable_name";
-    }
-}
-
-switch ($choice.ToLower())
-{
-    "site" {
-        # Get a site name from the user & search for it
-        $sites = Get-MgSite -search "$(Read-Host -Prompt 'Site Display Name')";
-        # For each site, make a custom object with the site's index number & display it
-        $sites `
-            | % {$i=0}{ [PsCustomObject]@{Num=$i;Name=$_.DisplayName;URL=$_.WebUrl} ;$i++} `
-            | Format-Table;
-        # Then ask the user which index they'd like
-        $Global:site = $sites[$(Read-Host -Prompt "Your Chosen Site's num")];
-        # And output it for them :)
-        print_if_not_null $site "site"
-    }
-
-    "list" {
-        if ($site -eq $null) {
-            # Try get a site
-            ./get_graph.ps1 "site"
-            if ($site -eq $null) { return }
-        }
-        Write-Host "`nGetting lists from site '$($site.DisplayName)'";
-        $__filter = Read-Host -Prompt 'Display Name Filter (blank for all)';
-        $siteLists = Get-MgSiteList -SiteID $site.ID -All | Where-Object { $_.DisplayName -like "*$($__filter)*" };
-        $siteLists `
-            | % {$i=0}{ [PsCustomObject]@{Num=$i;Name=$_.DisplayName;URL=$_.WebUrl} ;$i++} `
-            | Format-Table;
-        $Global:list = $siteLists[$(Read-Host -Prompt "Your Chosen List's num")];
-        print_if_not_null $list "list"
-    }
-
-    "listitems" {
-        if ($site -eq $null) {
-            Write-Host "`$site is null!  Exiting";
-            return
-        }
-        if ($list -eq $null) {
-            Write-Host "`$list is null!  Exiting";
-            return
-        }
-        $Global:listItems = Get-MgSiteListItem -SiteId $site.id -ListId $list.id -All -ExpandProperty Fields
-        Write-Host "Stored $($listItems.length) items from '$($list.DisplayName)' into `$listItems"
-    }
-
-    # Remember to add any nre items to the start of file options list!
-    default {
-        __found_nothing
-    }
-}
-
 
 # Remove-Variable __found_nothing
 # Remove-Variable print_if_not_null
