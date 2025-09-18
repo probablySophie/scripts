77db3de6d0f0e18620b5687d790c75980fcf97ea
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2025-09-18 16:56:32 +0800

    half a million file types

--- microsoft/ListFormatting/ListView/DocumentList.ts
+++ microsoft/ListFormatting/ListView/DocumentList.ts
@@ -19,33 +19,34 @@ let base: RowFormatter = {
 	},
 }
 
-function ifs()
-{
-	
+const file_type_mappings: Record<string, string[]> = {
+	"link": ["url", "link"],
+	"video": ["mp4"],
+	"pdf": ["pdf"],
+	"txt": ["txt"],
+	"zip": ["zip"],
+	"spo": ["aspx"],
+	"xlsx": ["xls", "xlsx", "xlb", "xlc", "xlsb", "xlsm", "xlm", "xla", "xll", "xlam", "xlw", "excel", "csv", "odc", "ods"],
+	"xltx": ["xlt", "xltx", "xltm"],
+	"docx": ["doc", "docx", "docm", "odt", "docb", "wbk", "rtf", "word"],
+	"dotx": ["dot", "dotx", "dotm"],
+	"photo": ["bmp", "dib", "gif", "ico", "jfif", "jpe", "jpeg", "jpg", "png", "svg", "tif", "tiff", "xbm", "xpm"],
+	"pptx": ["pptx", "ppt", "ppam", "ppsx", "ppsm", "sldx", "sldm", "ppa", "pps", "pptm", "odp", "powerpoint"],
+	"potx": ["pot", "potx", "potm"],
+	"html": ["htm", "html", "css"],
+	"email": ['eml', 'msg', 'oft', 'ost', 'pst']
 }
 
+// https://github.com/microsoft/fluentui/blob/master/packages/react-file-type-icons/src/FileTypeIconMap.ts
 base.rowFormatter.children?.push({
 	elmType: "div",
 	style: { width: "32px", height: "32px", "margin-right": "10px", "flex-shrink": "0" },
 	children: [
 		Img("='" + icon_type_icon_url( "' + " + switch_val(
 	"[$File_x0020_Type]",
-	[
-		{ cmp_val: "'url'", result: "'link'" },
-		{ cmp_val: ["'mp4'"], result: "'video'" },
-		{ cmp_val: ["'pdf'"], result: "'pdf'" },
-		{ cmp_val: ["'txt'"], result: "'txt'" },
-		// Spreadsheets
-		{ cmp_val: ["xls", "xlsx", "xlb", "xlc", "xlsb", "xlsm", "xlt", "xltx", "xltm", "xlm", "xla", "xll", "xlam", "xlw", "excel", "csv", "odc", "ods"].map(t => `'${t}'`), result: "'xlsx'" },
-		// Text Documents
-		{ cmp_val: ["doc", "docx", "docm", "dot", "dotx", "dotm", "odt", "docb", "wbk", "rtf", "word"].map(t => `${t}`), result: "'docx'" },
-		// Images
-		{ cmp_val: ["bmp", "dib", "gif", "ico", "jfif", "jpe", "jpeg", "jpg", "png", "svg", "tif", "tiff", "xbm", "xpm"].map(t => `'${t}'`), result: "'photo'" },
-		// Presentations
-		{ cmp_val: ["pptx", "ppt", "pot", "potx", "potm", "ppam", "ppsx", "ppsm", "sldx", "sldm", "ppa", "pps", "pptm", "odp", "powerpoint"].map(t => `'${t}'`), result: "'pptx'" },
-		// Pages
-		{ cmp_val: ["htm", "html", "aspx", "css"].map(t => `'${t}'`), result: "'html'" },
-	],
+	Object.keys(file_type_mappings).map( key => {
+		return { result: `'${key}'`, cmp_val: file_type_mappings[key].map( s => `'${s}'` ) }
+	} ),
 	`'genericfile'`
 ) + " + '" ) + "'", { style: { "padding": "2px" } } ),
 	]
