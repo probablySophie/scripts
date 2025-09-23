6cfde9362fb9ce0ae6e19d081f8f4330c6c2bdf0
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2025-09-23 12:05:01 +0800

    comments & todoos

--- powershell/pnp/export_list_views.ps1
+++ powershell/pnp/export_list_views.ps1
@@ -1,6 +1,14 @@
 
+# TODO: Take an optional list & optional connection as arguments
+# TODO: Optionally save the list & connection for later use
+
 $views = Get-PnPView -List $pnplist
 $Columns = Get-PnPField -List $pnplist | % { [PSCustomObject]@{ DisplayName=$_.Title; InternalName = $_.InternalName; CustomFormatter=$_.CustomFormatter; Type= $_.TypeAsString; Required = $_.Required; Choices=$_.Choices; DefaultValue=$_.DefaultValue; Description=$_.Description } }
+
+$export_list_views = [PSCustomObject]@{
+    List=$pnplist;
+    Connection;
+}
 
 $dir_name = "List_$($pnplist.title)";
 
@@ -12,6 +20,7 @@ if ( (Get-Item ".\$dir_name" -ErrorAction SilentlyContinue) -eq $null )
     New-Item -Name $dir_name -ItemType Directory
 }
 
+# INFO: Exporting the list views
 foreach ($view in $views)
 {
     $ViewFields = $view.ViewFields | % { $_ }
@@ -38,10 +47,12 @@ foreach ($view in $views)
     } | ConvertTo-Json -Depth 10 > ".\$dir_name\View_$($view.Title).json";
 }
 
+# INFO: Exporting the list's content types - the content types store the new item field customisations
 foreach ($content_type in $content_types)
 {
     $content_type_fields = Get-PnpProperty -ClientObject $content_type Fields
     $content_type_field_links = Get-PnpProperty -ClientObject $content_type FieldLinks
+    # TODO: Also export the actual content type itself so we can add custom content types to wherever we're importing this to
 
     [PSCustomObject]@{
         Name = $content_type.Name;
@@ -49,6 +60,7 @@ foreach ($content_type in $content_types)
     } | ConvertTo-Json  -Depth 10  > ".\$dir_name\ContentType_$($content_type.Name).json";
 }
 
+# INFO: Exporting the list's columns
 [PSCustomObject]@{
     Columns=$Columns;
     Title=$pnplist.title;
