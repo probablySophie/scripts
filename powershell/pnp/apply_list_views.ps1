599ea9fbe49473d1d9318c1099f879bd7a6603c2
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2025-09-23 12:04:53 +0800

    todos

--- powershell/pnp/apply_list_views.ps1
+++ powershell/pnp/apply_list_views.ps1
@@ -1,4 +1,7 @@
 
+# TODO: Take an optional list & optional connection as arguments
+# TODO: Optionally save the list & connection for later use
+
 $connection = $connection2;
 $site = $site2;
 $list = $list2;
@@ -11,6 +14,8 @@ $ListInfo = Get-Content ".\$dir_name\List.json" | Out-String | ConvertFrom-Json;
 
 $existing_fields = Get-PnPField -List $list -Connection $connection;
 $i = 0;
+
+# INFO: Importing the columns
 foreach ( $column in $ListInfo.Columns )
 {
     $i++;
@@ -19,7 +24,7 @@ foreach ( $column in $ListInfo.Columns )
 
     $existing_column = ( $existing_fields | ? { $_.InternalName -eq $column.InternalName } );
 
-    # The column DOESN'T already exist
+    # If the column doesn't exist - make it
     if ( $existing_column -eq $null )
     {
         Add-PnPField -List $list `
@@ -38,6 +43,7 @@ foreach ( $column in $ListInfo.Columns )
         DisplayName=$column.DisplayName;
         DefaultValue=$column.DefaultValue;
     }
+    # Check if there's actually anything worth changing
     $Values.Keys | % {
         $key = $_;
         if ( ($existing_column.PSObject.Properties | ? { $_.Name -eq "$key" }) -eq $null ) {
@@ -47,11 +53,12 @@ foreach ( $column in $ListInfo.Columns )
             $differs = $true;
         }
     }
+    # If there are things to change - do it
     if ( $differs ) {
-    Set-PnPField -List $list `
-        -Identity $column.InternalName`
-        -Connection $connection `
-        -Values $Values
+        Set-PnPField -List $list `
+            -Identity $column.InternalName`
+            -Connection $connection `
+            -Values $Values
     }
 }
 
@@ -63,6 +70,7 @@ $view_files = $dir_contents | ? { $_.StartsWith("View_") }
 $content_type_files = $dir_contents | ? { $_.StartsWith("ContentType_") }
 
 $i = 0;
+# INFO: Importing the views
 foreach ( $file in $view_files )
 {
     $i++;
@@ -103,6 +111,7 @@ foreach ( $file in $view_files )
         }
     $differs = $false;
 
+    # Do we actually need to make any changes
     $Values.Keys | % {
         $key = $_;
         if ( ($existing_view.PSObject.Properties | ? { $_.Name -eq "$key" }) -eq $null ) {
@@ -123,6 +132,7 @@ foreach ( $file in $view_files )
     }
 }
 
+# Updating content types - useful for the new item form
 $i = 0;
 foreach ( $file in $content_type_files )
 {
