090f6dd047539b9d7c1f6d8b6711dfbbea31e5a1
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2026-05-07 09:09:07 +0800

    install Visio

---
+++ microsoft/installers/Visio.ps1
@@ -0,0 +1,22 @@
+# This is based HEAVILY off of https://github.com/MSEndpointMgr/M365Apps/blob/main/VisioWin32App/InstallVisio.ps1
+
+# WARN: This requires you have a Visio.xml config file seated next to this file
+
+$temp_path = "$($env:SystemRoot)\Temp\OfficeSetup";
+
+if ( Test-Path "$temp_path" ) {
+	Remove-Item -Path "$temp_path" -Recurse -Force -ErrorAction SilentlyContinue
+}
+
+$setup_path = (New-Item -ItemType "directory" -Path "$($env:SystemRoot)\Temp" -Name OfficeSetup -Force).FullName
+
+$setup_exe_path = Join-Path "$setup_path" "\setup.exe";
+
+curl.exe "https://officecdn.microsoft.com/pr/wsus/setup.exe" --output "$setup_exe_path"
+if ( -not ( Test-Path "$setup_exe_path" ) ) {
+	throw "Download of setup.exe failed";
+}
+
+Copy-Item "$($PSScriptRoot)\Visio.xml" $setup_path -Force -ErrorAction Stop
+
+Start-Process "$setup_exe_path" -ArgumentList "/configure $( Join-Path $setup_path "Visio.xml" )" -Wait -PassThru
